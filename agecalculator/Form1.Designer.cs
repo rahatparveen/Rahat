cfc61a1c9bb765925c77586792e73770bbed03b9
RASHIDA PARVEEN <RASHIDA PARVEEN@LAPTOP-NT4PIP4D>
2017-05-06 11:32:09 +0500

    color changed

--- agecalculator/Form1.Designer.cs
+++ agecalculator/Form1.Designer.cs
@@ -55,12 +55,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
             this.button1.Location = new System.Drawing.Point(321, 173);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "calculate age";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
